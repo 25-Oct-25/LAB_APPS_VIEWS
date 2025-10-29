bf1de8217c681457a3570af6199eb4668dd91914
Abdulrahman Alhrbi <AbdullrahmanTurki@outlook.com>
2025-10-29 11:26:37 +0300

    Lab 2 using django

---
+++ CarsRental/Main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse,HttpRequest
+import random
+import string
+
+# Create your views here.
+
+def home_view (request:HttpRequest):
+    content = "Hello World, This is my new HOME for Car Rentals Website  ! we're excited to welcome you here."
+    return HttpResponse(content)
+
+def about_view(request:HttpRequest):
+    content = "Car rental services provide temporary access to a diverse range of vehicles—from economy cars to luxury models—offering travelers and individuals a flexible, convenient solution for mobility and transportation needs."
+    return HttpResponse(content)
+
+def password_generate(request:HttpRequest):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(10))
+    content = f"Your generated password is : {password}"
+    return HttpResponse(content)
